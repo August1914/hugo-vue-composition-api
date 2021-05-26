b047d613f62823fd7dd14dc889d8e40b799123f5
ClaudiaGiv <clau_givan@yahoo.com>
2021-05-26 14:54:17 +0300

    Add fauna db add-on for netlify

--- src/vue/card.vue
+++ src/vue/card.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Cards</h1>
+    <p>This is the card component!</p>
+    <h2>card component!!!YESSSS</h2>
     <p>This is a card: <b>{{cardName}}</b></p>
     <p>Card description: <b>{{cardDescription}}</b></p>
 <!--    <p>[[cardName]]</p>-->
@@ -8,6 +10,7 @@
 </template>
 
 <script>
+
 import cardJson from "./../../data/card.json";
 export default {
   name: "card",
